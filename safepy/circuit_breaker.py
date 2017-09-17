d9d6f4eda01668ad18085512cc1d6d17ce8fbc6f
Stanislav Prokop <prokopst@stanislav-prokop-macos.local>
2017-09-17 15:18:38 +0200

    [WIP] Moar code!

--- safepy/circuit_breaker.py
+++ safepy/circuit_breaker.py
@@ -1,6 +1,7 @@
 import collections
 import time
 from asyncio.locks import Lock
+from enum import Enum
 
 
 class StatisticsBucket:
@@ -9,40 +10,79 @@ class StatisticsBucket:
         self.failed = 0
 
 
-class CicruitBreakerStatistics:
-    def __init__(self, buckets_count, bucket_interval):
-        self._deque = collections.deque(maxlen=buckets_count)
+class StatisticsBucketContainer:
+    def __init__(self, size):
+        # TODO: value sanity check
+        self._queue = collections.deque(maxlen=size)
+
+    def reset_from_left(self, count):
+        real_count = min(count, len(self._queue))
+        for _ in range(real_count):
+            self._queue.popleft()
+            self._queue.append(StatisticsBucket())
+
+    def update_current(self, passed, failed):
+        current_bucket = self._queue[0]
+        current_bucket.passed += passed
+        current_bucket.failed += failed
+
+    def evaluate(self):
+        passed = 0
+        failed = 0
+
+        for bucket in self._queue:
+            passed += bucket.passed
+            failed += bucket.failed
+
+        return passed, failed
+
+
+class CircuitBreakerState(Enum):
+    closed = 1
+    open = 2
+    half_open = 3
+
+
+class CircuitBreaker:
+    def __init__(self, func, buckets_count, bucket_interval, min_evaluation):
+        self._bucket_container = StatisticsBucketContainer(buckets_count)
         self._bucket_interval = bucket_interval
         self._interval = self._bucket_interval * buckets_count
-        self._start_time = time.monotonic()
+        self._next_time = time.monotonic() + self._bucket_interval
         self._lock = Lock()
+        self._func = func
+        self._min_evaluation = min_evaluation
+        self._current_state = CircuitBreakerState.closed
 
-    async def add_stats(self, passed, failed):
+    def _handle_closed(self):
+        pass
+
+    def _handle_open(self):
+        pass
+
+    def _handle_half_open(self):
+        pass
+
+    async def call(self, *args, **kwargs):
+
         with await self._lock:
             current_time = time.monotonic()
 
-            if current_time > (self._start_time + self._interval):
-                self._deque.popleft()
-                # TODO: change the start time
-                bucket = StatisticsBucket()
-                self._deque.append(bucket)
+            if current_time > self._next_time:
+                quotient, remainder = divmod(current_time - self._next_time, self._bucket_interval)
+                if remainder > 0:
+                    quotient += 1
 
-            elif self._deque:
-                bucket = self._deque[-1]
+                self._next_time = quotient * self._bucket_interval
+                self._bucket_container.reset_from_left(quotient)
 
-            else:
-                bucket = StatisticsBucket()
-                self._deque.append(bucket)
+                passed, failed = self._bucket_container.evaluate()
 
-            bucket.passed += passed
-            bucket.failed += failed
+                count = passed + failed
 
-    async def evaluate(self):
-        with await self._lock:
-            passed, failed = 0, 0
-
-            for bucket in self._deque:
-                passed += bucket.passed
-                failed += bucket.failed
-
-            return passed, failed
+                if count > self._min_evaluation:
+                    if passed
+        try:
+            return await self._func(*args, **kwargs)
+        finally:
+            pass
